3ea8e7058aeb7212ce09d93c485d981047cdc256
NipunSGeeTH <sangeethnipun385@gmail.com>
2025-09-01 14:11:17 +0530

    chore:init web app and add tailwind css

---
+++ tailwind.config.ts
@@ -0,0 +1,12 @@
+import type { Config } from 'tailwindcss'
+
+export default {
+  content: [
+    "./src/**/*.{astro,html,js,jsx,svelte,ts,tsx,vue}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+} satisfies Config
+
